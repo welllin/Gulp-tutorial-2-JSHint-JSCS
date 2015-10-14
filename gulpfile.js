69dee83ddbd40a9d541a3322fc82e6ff331be105
Well <309635368@qq.com>
2015-10-14 18:36:59 +1300

    Css Compiliation

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     jscs = require('gulp-jscs');
+    sass = require('gulp-sass');
+    del = require('del');
 
 gulp.task('check', function(){
   return gulp
@@ -11,3 +13,15 @@ gulp.task('check', function(){
       .pipe(jshint())
       .pipe(jshint.reporter());
 });
+
+gulp.task('style', ['clean-styles'], function(){
+  return gulp
+      .src('./asset/css/*.scss')
+      .pipe(sass())
+      .pipe(gulp.dest('./.tmp'));
+});
+
+gulp.task('clean-styles', function(){
+  var files = '.tmp/*.css';
+  del(files);
+})
